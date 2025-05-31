682a71070473ae2624c22e2384fb0f0eb85c3a12
Estebanacuna <estebanac43@gmail.com>
2025-05-30 22:33:08 -0300

    Crear nueva carpeta ProT2_42203444

---
+++ ProT2_42203444/app/Views/front/footer_view.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="footer-content">
+        <p>Derechos de autor 2025 Mi página web. Todos los derechos reservados</p>
+        <div>
+                  
+        </div>
+        
+    </div>
+    </footer>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
